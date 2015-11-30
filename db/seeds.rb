fbf1dbc73fadd34f8cf48572768b7f3e5ba70f92
sebaswn <sebastiannee@yahoo.com>
2015-11-30 18:12:16 +0100

    trimming down assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ User.create(email: "sebas@fishtank.h20",
 	puts("----------------------------------------------------------------")
 
 
-30.times do |i|
+20.times do |i|
 	
 	Post.create( name: Faker::Team.name, 
 					location: Faker::Address.city, 
